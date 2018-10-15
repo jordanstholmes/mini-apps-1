993c19897474fbe723e5e415a95adc383f187726
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-15 11:16:32 -0700

    Added buttons for squares and reset button

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,9 @@
+document.getElementById('game-board');
+
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', (e) => {
+  console.log('reset was clicked!');
+});
 
 
