b2f1c29a1b55ae2fe63b85c4cc28fb88ac4dfd07
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-15 12:21:03 -0700

    Add virtual gamestate where x is represented as 1 and o as -1

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,30 +3,60 @@ const resetButton = document.getElementById('reset');
 const gameBoard = document.getElementById('game-board');
 
 const GameState = {
-  lastMove: 'X',
+  lastMove: -1,
+  board: [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+  ],
+  toggleMove: function([row, col]) {
+    GameState.board[row][col] = GameState.lastMove;
+  },
+  reset: function() {
+    for (var row = 0; row < GameState.board.length; row++) {
+      for (var col = 0; col < GameState.board.length; col++) {
+        GameState.board[row][col] = 0;
+      }
+    }
+  },
   getNextMoveType: function() {
-    GameState.lastMove = GameState.lastMove === 'x' ? 'o' : 'x';
-    return GameState.lastMove;
+    GameState.lastMove = GameState.lastMove === 1 ? -1 : 1;
+    return GameState.lastMove === 1 ? 'x' : 'o';
+  },
+  _checkVerticalWin: function() {
+
+  },
+  _checkHorizontalWin: function() {
+
+  },
+  _checkMajorDiagonalWin: function() {
+
+  },
+  _checkMinorDiagonalWin: function() {
+
   }
+
 }
 
 const Controller = {
   addListeners: () => {
     resetButton.addEventListener('click', (e) => {
-      const table = document.getElementById('game-board');
-      allSquares = table.getElementsByTagName('button');
+      // const table = document.getElementById('game-board');
+      // allSquares = table.getElementsByTagName('button');
 
-      for (var i = 0; i < allSquares.length; i++) {
-        allSquares[i].innerHTML = '&nbsp;';
-      }
+      // for (var i = 0; i < allSquares.length; i++) {
+      //   allSquares[i].innerHTML = '&nbsp;';
+      // }
 
-      console.log(allSquares);
+      // GameState.toggleMove(1, 1);
+      console.log(GameState.board);
     });
     
     gameBoard.addEventListener('click', (e) => {
       const square = e.target;
       if (square.innerHTML === '&nbsp;') {
         square.innerHTML = GameState.getNextMoveType();
+        GameState.toggleMove(square.id.split(''));
       }
     });
   }
