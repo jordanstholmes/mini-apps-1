ea82c65c8089347c9273186869db239fef0c4af0
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-15 11:28:48 -0700

    Got x and o alternating

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,9 +1,30 @@
 document.getElementById('game-board');
-
 const resetButton = document.getElementById('reset');
+const gameBoard = document.getElementById('game-board');
 
-resetButton.addEventListener('click', (e) => {
-  console.log('reset was clicked!');
-});
+const GameState = {
+  lastMove: 'X',
+  getNextMoveType: function() {
+    GameState.lastMove = GameState.lastMove === 'X' ? 'O' : 'X';
+    return GameState.lastMove;
+  }
+}
+
+const Controller = {
+  addListeners: () => {
+    resetButton.addEventListener('click', (e) => {
+
+      console.log(GameState.getNextMoveType());
+    });
+    
+    gameBoard.addEventListener('click', (e) => {
+      console.log(e.target);
+    });
+  }
+
+}
+
+Controller.addListeners();
+
 
 
