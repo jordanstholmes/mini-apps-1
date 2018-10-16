463366b698c63bd468e5e494f39071a296aeb37c
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-16 11:27:49 -0700

    Create basic form in html

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const port = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+
+app.listen(port, () => {
+  console.log(`listening at ${port}...`);
+});
 
 
