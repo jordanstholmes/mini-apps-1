72abe8079956aed1fe4e2b649bcfab43971924d7
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-16 14:06:19 -0700

    Pass basic test of sending client's json text to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
+const multer = require('multer');
+const upload = multer();
+
 
 const port = 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client')));
+
+app.post('/handle-json', upload.none(), (req, res, next) => {
+  console.log(req.body);
+});
 
 
 app.listen(port, () => {
