0b0bb69b5cfee6e01528fcf6eff86e3485ec08c9
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-17 14:54:26 -0700

    Automate adding and reseting dummy data in db

---
+++ challenge_3/dummyData.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: 'ted',
+    email: 'ted@gmail.com', 
+    password:'123',
+    address: {
+      line: '',
+      line2: '',
+      city: 'SF',
+      state: 'CA',
+      zip: 94901
+    },
+    cc: {
+      number: 92572093,
+      exp: '11/21/2019',
+      CVV: 214,
+      zip: 94901
+    }
+  },
+  {name: 'jane', email: 'jane@yahoo.com', password:'luck67'},
+  {name: 'valerie', email: 'valerie@hotmail.com', password:'p@ssword'}
+];
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,9 +1,81 @@
+/****************************************************
+MODULE IMPORTS
+****************************************************/
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('mongodb');
+const dummyDataArray = require('./dummyData.js');
+const _ = require('underscore');
+/****************************************************
+GET CONNECTION TO DATABASE
+****************************************************/
+const dbURL = 'mongodb://localhost:27017';
+const dbName = 'checkout';
+var db;
+
+MongoClient.connect(dbURL, (err, client) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Successfully connected to mongod server...');
+
+  db = client.db(dbName);
+  console.log('DB EXISTS:', db instanceof mongodb.Db);
+
+  resetDummyData();//
+
+});
+
+/****************************************************
+DATABASE TESTER FUNCTIONS
+****************************************************/
+
+function resetDummyData() {
+  deleteCollection('transactions', () => {
+    const collection = db.collection('transactions');
+    console.log('COLLECTION EXISTS:', collection instanceof mongodb.Collection);
+
+    collection.insertMany(dummyDataArray, (err, dbRes) => {
+      console.log('DUMMY DATA ADDED:', dbRes.result.ok === 1); //
+    });
+  });
+}
+
+function deleteCollection(collectionName, callback) {
+  const collection = db.collection(collectionName);
+  collection.drop((err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`DROP OLD DUMMY:`, result);
+    callback();
+  });
+}
+
+function checkDummyData(collectionName, callback) {
+  const collection = db.collection(collectionName); 
+  collection.find({}).toArray((err, docs) => {
+    console.log('FOUND CORRECT DUMMY DATA:', docs.length === dummyDataArray.length);
+  })
+}
+
+/****************************************************
+EXPRESS SETUP APP
+****************************************************/
+
 const port = 3000;
 const app = express();
 
+/****************************************************
+API
+****************************************************/
 
 app.get('/', (req, res) => res.send('hello world'));
 
-app.listen(port, () => console.log(`server listening on port ${port}...`));
+/****************************************************
+START EXPRESS SERVER
+****************************************************/
 
+app.listen(port, () => console.log(`server listening on port ${port}...`));
