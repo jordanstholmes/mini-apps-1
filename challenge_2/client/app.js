e59c59dd22beb751f10805e2a9faacfc71ee9b24
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-16 19:19:00 -0700

    Start work on refactoring form submit to use ajax

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,30 @@
+const $ = jQuery;
+const options = {
+  url: 'http://localhost:3000/handle-json',
+  data: undefined,
+  method: 'POST'
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  // var $convertBtn = $('#convert-btn');
+
+  // $convertBtn.click((e) => {
+
+  // })
+  var $converterForm = $('#json-converter');
+  $converterForm.submit( e => {
+    var $file = $('input:file');
+    options.data = $file;
+
+    $.ajax(options)
+
+    console.log('form submited');
+    console.log($file);
+
+    e.preventDefault();
+  });
+  // console.log($converterForm);
+
+});
 
 
