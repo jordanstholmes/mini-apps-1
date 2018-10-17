0d4c8596fdc07dfc5e042cfa43b35125a712e528
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-17 15:04:29 -0700

    Refactor react components to include a parent App element

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,14 +1,28 @@
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {form: 'checkout'}
+  }
+
+  render() {
+    return (
+      <div id='app'>
+        <NavButton />
+        <F1 />
+      </div>
+    );
+  }
+}
 
 function NavButton() {
   return (
     <div>
       <button>checkout</button>
-      <NameEmailPassword />
     </div>
   );
 }
 
-function NameEmailPassword() {
+function F1() {
   return (
     <form>
       <label>Name</label><input type='text'></input>
@@ -18,7 +32,5 @@ function NameEmailPassword() {
   );
 }
 
-
-
-ReactDOM.render(<NavButton />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
