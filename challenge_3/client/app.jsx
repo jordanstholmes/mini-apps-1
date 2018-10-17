1e3d72427313450ec88da5b2dc751ad2e99333ee
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-17 11:49:37 -0700

    Set up babel transpiler w/ react preset

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,8 @@
+const React = require('react');
 
+function testComponent() {
+  return (
+    <h1>I am a test react element</h1>
+  );
+}
 
---
+++ challenge_3/lib/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const React = require('react');
+
+function testComponent() {
+  return React.createElement("h1", null, "I am a test react element");
+}
